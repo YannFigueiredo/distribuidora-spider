e15cf0960c6b761e474003d4e6a4fa69a7cb22f4
Yann Figueiredo <yann.fabricio@hotmail.com>
2023-08-25 10:45:07 -0300

    chore: adicionada conexão ao banco de dados e .env no gitignore

--- backend/src/database/connect.js
+++ backend/src/database/connect.js
@@ -1,5 +1,26 @@
 import mysql2 from "mysql2/promise";
+import dotenv from "dotenv";
 
-const connect = mysql2.createPool({
-  
+dotenv.config();
+
+const connection = mysql2.createPool({
+  user: process.env.DB_USERNAME,
+  password: process.env.DB_PASSWORD,
+  host: process.env.DB_HOST,
+  database: process.env.DB_NAME,
+  port: process.env.DB_PORT
 });
+
+const connect = async () => {
+  try {
+    const connectionStatus = await connection.getConnection();
+
+    console.log("Banco de dados conectado com sucesso!");
+
+    connectionStatus.release();
+  } catch(e) {
+    console.log("Erro ao conectar no banco de dados: ", e);
+  }
+};
+
+export default connect;
--- backend/src/index.js
+++ backend/src/index.js
@@ -3,6 +3,7 @@ import productTypeRouter from "./routes/ProductTypeRoutes.js";
 import productRouter from "./routes/ProductRoutes.js";
 import categoryRouter from "./routes/CategoryRoutes.js";
 import supplierRouter from "./routes/SupplierRoutes.js";
+import connect from "./database/connect.js";
 
 const app = express();
 
@@ -12,5 +13,7 @@ app.use(productTypeRouter);
 app.use(productRouter);
 app.use(categoryRouter);
 app.use(supplierRouter);
+
+connect();
 
 export default app;
