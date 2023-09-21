57ad2f8e54ada8f542d36587a1ac261b270f57ce
Yann Figueiredo <yann.fabricio@hotmail.com>
2023-09-21 17:26:46 -0300

    feat: adicionadas restrições a tabelas do banco de dados

--- backend/src/models/associations.js
+++ backend/src/models/associations.js
@@ -6,9 +6,9 @@ import CategoryModel from "./CategoryModel.js";
 import SupplierModel from "./SupplierModel.js";
 
 export const setupAssociations = () => {
-  OrderModel.belongsToMany(ProductModel, { through: OrderProductModel });
-  ProductModel.belongsToMany(OrderModel, { through: OrderProductModel });
-  ProductModel.belongsTo(ProductTypeModel, { as: "productType" });
+  OrderModel.belongsToMany(ProductModel, { through: OrderProductModel, onDelete: "CASCADE", onUpdate: "CASCADE" });
+  ProductModel.belongsToMany(OrderModel, { through: OrderProductModel, onDelete: "CASCADE", onUpdate: "CASCADE" });
+  ProductModel.belongsTo(ProductTypeModel, { as: "productType", onDelete: "RESTRICT" });
   ProductTypeModel.belongsTo(CategoryModel, { as: "category" });
   ProductTypeModel.belongsTo(SupplierModel, { as: "supplier" });
 };
